2690d108c7cb8532a9095bd923e1ecf8370a290f
comnows <20591act@gmail.com>
2022-11-18 19:09:06 +0700

    ADD Interact module

---
+++ VR_ShooterGame/Assets/Game/General/Script/Interactable/SlidingDoor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoor : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Door opening");
+    }
+}
---
+++ VR_ShooterGame/Assets/Game/General/Script/Player/PlayerInteract.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+
+    private Ray interactRay;
+    private RaycastHit hitInfo;
+
+    private Color rayColor = Color.blue;
+    
+    [SerializeField] private float interactRange = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayColor = Color.blue;
+        
+        interactRay = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+        //can add masking for raycast
+        if(Physics.Raycast(interactRay, out hitInfo, interactRange))
+        {
+            Interactable interactable = hitInfo.transform.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                //don't for get if key is pressed
+                interactable.Interact();
+            }
+
+            rayColor = Color.yellow;
+            Debug.Log(hitInfo.transform.name);
+        }
+
+        Debug.DrawRay(interactRay.origin, interactRay.direction * interactRange, rayColor);
+    }
+}
